b114e7c7d663f9da60be03cdbabaaff372fdf881
xudyang1 <61672396+xudyang1@users.noreply.github.com>
2024-05-01 23:12:16 -0400

    refactor: use table to hold all exports

--- lua/redir/init.lua
+++ lua/redir/init.lua
@@ -1,31 +1,17 @@
 local Config = require("redir.config")
 local View = require("redir.view")
 
-local Redir = {}
-
-function Redir.setup(opts)
+local function setup(opts)
   Config.setup(opts)
   View.setup()
 end
 
-function Redir.open_win()
-  View:open_win()
-end
-
-function Redir.open_float()
-  View:open_float()
-end
-
-function Redir.open_vertical()
-  View:open_vertical()
-end
-
-function Redir.open_horizontal()
-  View:open_horizontal()
-end
-
-function Redir.open_tab()
-  View:open_tab()
-end
-
-return Redir
+return {
+  setup = setup,
+  open_win = View.open_win,
+  open_float = View.open_float,
+  open_vertical = View.open_vertical,
+  open_horizontal = View.open_horizontal,
+  open_tab = View.open_tab,
+  close_win = View.close_win,
+}
